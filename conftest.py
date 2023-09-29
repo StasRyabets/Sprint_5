b9ad9a1d11729b26ef1156e0ddc9269d691436d4
Stas Ryabets <stas.ryabets@mail.ru>
2023-09-29 14:24:35 +0500

    delete unnecessary param

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     driver.set_window_size(1920, 1080)
